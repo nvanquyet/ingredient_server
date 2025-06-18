789406ee7466118a018ed07ba0fd12ecb0c7820a
VawnWuyest <nvanquyet810@gmail.com>
2025-06-18 14:42:46 +0700

    [Fix] Add namespace

--- IngredientServer/API/Program.cs
+++ IngredientServer/API/Program.cs
@@ -8,9 +8,11 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using IngredientServer.Core.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
